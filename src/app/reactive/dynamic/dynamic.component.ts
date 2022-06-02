b45dc555e9a20bd034d4d8c7636fc669bd0b6db0
Oscar Monroy <oscarmonroy657@gmail.com>
2022-06-01 19:24:49 -0600

    Dynamic form with reactive forms is working.

--- src/app/reactive/dynamic/dynamic.component.ts
+++ src/app/reactive/dynamic/dynamic.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic',
@@ -6,11 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DynamicComponent implements OnInit {
+export class DynamicComponent{
+  myForm: FormGroup = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favorites: this.formBuilder.array([
+      ['Metal Slug'],
+      ['Super mario bros']
+    ], Validators.required)
+  })
 
-  constructor() { }
+  newFavorite: FormControl = this.formBuilder.control('', [Validators.required]);
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) { }
+
+  get favorites(): FormArray {
+    return this.myForm.get('favorites') as FormArray;
   }
 
+  inputIsValid(input: string): boolean {
+    return !!this.myForm.controls[input].errors && this.myForm.controls[input].touched;
+  }
+
+  save(): void {
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+    this.myForm.reset();
+    this.favorites.clear();
+  }
+
+  addFavorite(): void {
+    if(this.newFavorite.invalid){
+      return;
+    }
+    // this.favorites.push(new FormControl(this.newFavorite.value, Validators.required));
+    this.favorites.push(this.formBuilder.control(this.newFavorite.value, Validators.required));
+    this.newFavorite.reset();
+  }
+
+  deleteFavorite(index: number): void {
+    this.favorites.removeAt(index);
+  }
 }
