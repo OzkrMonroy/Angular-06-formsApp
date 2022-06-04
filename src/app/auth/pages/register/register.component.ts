d8ca3137848ac96e84f6b88882b469082707c6b7
Oscar Monroy <oscarmonroy657@gmail.com>
2022-06-04 06:56:27 -0600

    Validatios for name field has been added.

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class RegisterComponent implements OnInit {
+  namePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
 
-  constructor() { }
+  myForm: FormGroup = this.formBuilder.group({
+    name: ['', [ Validators.required, Validators.pattern(this.namePattern) ]]
+  })
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  inputIsValid(input: string): boolean {
+    return !!this.myForm.controls[input].errors && this.myForm.controls[input].touched;
+  }
+
+  submit():void {
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return
+    }
+    console.log(this.myForm.value);
+    this.myForm.reset()
+    
+  }
 }
