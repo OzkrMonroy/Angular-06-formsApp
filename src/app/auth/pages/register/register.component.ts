8a4d079d2eba3068dc72038aed64ff2e949ad05b
Oscar Monroy <oscarmonroy657@gmail.com>
2022-06-04 07:05:41 -0600

    Validations for email field have been added.

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -9,9 +9,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
   namePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
   myForm: FormGroup = this.formBuilder.group({
-    name: ['', [ Validators.required, Validators.pattern(this.namePattern) ]]
+    name: ['', [ Validators.required, Validators.pattern(this.namePattern) ]],
+    email: ['', [ Validators.required, Validators.pattern(this.emailPattern) ]]
   })
 
   constructor(private formBuilder: FormBuilder) { }
