69326b56244299118207369a65a3e6e4067e6397
Oscar Monroy <oscarmonroy657@gmail.com>
2022-06-04 05:55:50 -0600

    switch controls with reactive forms are ready.

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -7,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class SwitchesComponent implements OnInit {
+  myForm: FormGroup = this.formBuilder.group({
+    genre: ['M', Validators.required],
+    notifications: [true],
+    conditions: [false, Validators.requiredTrue]
+  })
 
-  constructor() { }
+  person = {
+    genre: 'F',
+    notifications: false,
+  }
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.myForm.reset({...this.person, conditions: true});
+
+    // If you want to keep the person object synchronized with the form you can use the following code:
+    this.myForm.valueChanges.subscribe({
+      next: ({ conditions, ...rest }) => {
+          this.person = {...rest};
+        }
+    });
+  }
+
+  save():void {
+    if(this.myForm.invalid){
+      return;
+    }
+    const formValues = {... this.myForm.value};
+    delete formValues.conditions;
+
+    this.person = formValues;
   }
 
 }
